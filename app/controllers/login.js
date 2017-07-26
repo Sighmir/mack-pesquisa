e91b807db67181499e45acd1145e478da9e01a26
Gustavo Silva <gustavo.silva@verity.com.br>
2017-07-26 09:54:44 -0300

    Ending login logic

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -35,20 +35,26 @@ module.exports = function(app){
                         res.render("login/login", {erro:erro});
                         return;
                   }
-                 bcrypt.compare(usuarioLogin.senha, resultado.senha, function(err, senhasCombinam){
+                 if(resultado[0]){
+                     bcrypt.compare(usuarioLogin.senha, resultado[0].senha, function(err, senhasCombinam){
                         if (senhasCombinam){
                             req.session.email = usuarioLogin.email;
-                            res.redirect("home/index"); 
+                            res.redirect("/home"); 
                             return;
                         }else{
-                            var erro = "Email ou senha incorretos!";
+                            var erro = "E-mail ou senha incorretos!";
                             res.render("login/login", {erro:erro});
                             return;
                         }
-                });   
+                    });   
+                 }else{
+                    var erro = "E-mail não existe, por favor realize seu cadastro!";
+                    res.render("login/login", {erro:erro});
+                    return;
+                 }
             })
         }else{
-            var erro = "Email ou senha incorretos!";
+            var erro = "E-mail ou senha incorretos!";
             res.render("login/login", {erro:erro});
             return;
         }
@@ -56,7 +62,8 @@ module.exports = function(app){
     });
 
     app.post("/cadastro", function(req, res){
-        
+        var usuario = req.body;
+
         var connection = new app.infra.ConnectionFactory();
         var usuarioDAO = new app.persistencia.UsuarioDAO(connection);
         
@@ -67,10 +74,49 @@ module.exports = function(app){
         var erros = req.validationErrors();
 
         if(erros){
-            req.session.destroy();
             var mensagemErro = "Por favor, preencha todos os campos corretamente!";
-            
+            res.status(500).json(mensagemErro);
             return;
+        }else{
+             usuarioDAO.buscarPorEmail(usuario, function(error, resultado){
+                 if(error){
+                     console.log("Erro de banco: "+error)
+                        var mensagemErro = "Erro ao realizar cadastro!";
+                        res.status(500).json(mensagemErro);
+                        return;
+                  }
+                 if(resultado[0]){
+                     console.log(resultado);
+                    var mensagemErro = "Já existe uma conta utilizando este e-mail!";
+                    res.status(500).json(mensagemErro);
+                    return;
+                 }else{
+                     
+                    bcrypt.hash(usuario.senha, 5, function(err, hash) {
+                        if(err){
+                            var mensagemErro = "Erro ao realizar cadastro!";
+                            res.status(500).json(mensagemErro);
+                            return;
+                        }else{
+
+                            console.log(hash);
+                            usuario.senha = hash;
+                            usuario.data_cadastro = new Date();
+                            usuarioDAO.inserir(usuario, function(erro, resultado){
+                                if(erro){
+                                    console.log("Erro de banco: "+erro)
+                                    var mensagemErro = "Erro ao realizar cadastro!";
+                                    res.status(500).json(mensagemErro);
+                                    return;
+                                }else{
+                                    res.status(201).json(resultado.insertId);
+                                    return;
+                                }
+                            });
+                        }
+                    });
+                 }
+            });
         }
 
         
--- app/public/js/login/main-login.js
+++ app/public/js/login/main-login.js
@@ -8,16 +8,51 @@ function trocaTela(event){
     if(event != undefined)event.preventDefault();
     $("#login").toggleClass("elemento-escondido");
     $("#cadastro").toggleClass("elemento-escondido");
+    $(".input").val("");
 }
 
 $("#cadastrar").click(function(event){
     event.preventDefault();
     var formularioValido = $("#cadastro form")[0].checkValidity();
+
+    var usuario = {
+        "nome" : $("#nome").val(),
+        "email" : $("#email-cadastro").val(),
+        "senha" : $("#senha-cadastro").val(),
+    }
+    
     if(formularioValido){
+       var dialog = {};
+
         $("#feedback").fadeOut();  
-        $("#feedback").toggleClass("elemento-escondido");
+        $("#feedback").addClass("elemento-escondido");
+
+        $.ajax({
+            url: "/cadastro",
+            method:"post", 
+            dataType:"json", 
+            data: usuario,
+            beforeSend: function(){
+                dialog = bootbox.dialog({
+                    title: 'Realizando operação',
+                    message: '<p><i class="fa fa-spin fa-spinner"></i> Aguarde...</p>'
+                });    
+            }, success:function(data){
+                console.log(data);
+                $("#cadastro-sucesso").removeClass("elemento-escondido");
+                trocaTela();
+                $("#email").val(usuario.email);
+            },error:function(error){
+                $("#erro-cadastro strong").text(error.responseText);
+                $("#erro-cadastro").removeClass("elemento-escondido");
+            },complete: function(){
+                dialog.modal('hide');
+            }
+
+        });
     }else{
-        $("#feedback").toggleClass("elemento-escondido");
+
+        $("#feedback").removeClass("elemento-escondido");
         $("#feedback").hide();
         $("#feedback").fadeIn();    
     }
